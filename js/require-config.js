4e76d2c9cc74606f62de847ef4c0e00912d2ab4e
alexbardas <alexbardas@gmail.com>
2012-05-17 02:00:42 +0300

    Made require config load first to not have problems

--- js/require-config.js
+++ js/require-config.js
@@ -1,13 +1,17 @@
-require.config({
+var require = {
+	baseUrl: "js",
 	paths: {
+		"slimscroll": "libs/slimscroll.jquery",
+		"jit": "libs/jit",
 		"backbone": "backbone-loader",
 		"cs": "libs/cs",
 		"log": "log",
 		"mustache": "mustache-loader",
 		"tree": "tree",
 		"treeview": "treeview",
+		"spaceTree": "spaceTree",
+		"jit": "libs/jit",
 		"owl": "ontology",
 		"underscore": "libs/underscore"
-	},
-	waitSeconds: 3
-})
+	}
+}
